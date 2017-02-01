c68ada79b6d8eb74deffa2ad415b920d79a41f99
Mehedee Hassan <razin.meha@gmail.com>
2017-02-01 11:10:42 +0600

    add -operations

--- Retro Calculator/Retro Calculator/ViewController.swift
+++ Retro Calculator/Retro Calculator/ViewController.swift
@@ -7,19 +7,153 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
+    
+    var buttonSound: AVAudioPlayer!
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    
+    var presentNumber = ""
+    
+    
+    enum Operation : String {
+        
+        case Devide = "/"
+        case Multiply  = "*"
+        case Substract = "-"
+        case Add = "+"
+        case Empty  = "NIL"
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        let path = Bundle.main.path(forResource: "btn",ofType: "wav")
+        
+        let soundUrl = URL(fileURLWithPath: path!)
+        
+        
+        
+        do{
+            
+            try buttonSound = AVAudioPlayer(contentsOf : soundUrl)
+            buttonSound.play()
+            
+        } catch let err as NSError{
+            print (err.debugDescription)
+        }
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func onNumberPressedPlay(sender: UIButton){
+        
+        presentNumber += "\(sender.tag)"
+        counterLabel.text = presentNumber
+        
+        playSound()
+        
+        
     }
 
+    
+    @IBAction func onEqualPress(sender: UIButton){
+        
+        
+    }
+    
+    @IBAction func onAddPress(sender: UIButton){
+        
+        print ("\n__TAG \(presentNumber)\n")
+        
+        
+        processOperation(operation: Operation.Add)
+    }
+    @IBAction func onSubtractPress(sender: UIButton){
+        
+    }
+    @IBAction func onMultiplyPress(sender: UIButton){
+        
+    }
+    @IBAction func onDevidePress(sender: UIButton){
+        
+    }
+    
+    
+    func playSound(){
+        if buttonSound.isPlaying {
+            buttonSound.stop()
+        }
+        
+        buttonSound.play()
+    }
+    
+    
+    var currentOperation = Operation.Empty
+    var rightNumber = "";
+    var leftNumber = "0";
+    var result = "";
+    
+    
+    func processOperation(operation :Operation){
+        
+        
+        
+        if operation != Operation.Empty{
+            
+            
+            
+            
+            if presentNumber != ""  {
+                
+                
+                
+                rightNumber = presentNumber
+                
+           
+                
+                print ("\npresent = \(presentNumber) \nright=\(rightNumber) \nleft = \(leftNumber)")
+                
+                
+                
+                if operation == Operation.Add {
+                    result  = "\(Double(leftNumber)! + (Double(rightNumber))!)"
+                    
+                }
+                
+                
+                counterLabel.text = result
+                leftNumber = result
+                
+                
+                presentNumber = "";
+                
+                
+                currentOperation = Operation.Empty;
+                
+            }
+            
+            
+            
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
 
 }
 
