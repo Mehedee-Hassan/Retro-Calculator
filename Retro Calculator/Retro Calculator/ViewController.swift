61c4ae5e1104e76555ca4f40d141efef0cabec0d
Mehedee Hassan <razin.meha@gmail.com>
2017-02-07 21:40:13 +0600

    logic change

--- Retro Calculator/Retro Calculator/ViewController.swift
+++ Retro Calculator/Retro Calculator/ViewController.swift
@@ -15,9 +15,6 @@ class ViewController: UIViewController {
     
     var buttonSound: AVAudioPlayer!
     
-    var equalPressed = false
-    var calculatorTurnedOn = true
-    var pressedFlag = -1 // 1= number ,2 = operation ,3 = equals
     
     
     @IBOutlet weak var counterLabel: UILabel!
@@ -37,7 +34,11 @@ class ViewController: UIViewController {
     
     
     
+    
+    
     override func viewDidLoad() {
+       
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     
@@ -57,30 +58,29 @@ class ViewController: UIViewController {
         }
         
         
+        
+        
     }
 
+
+    
+    var currentOperation = Operation.Empty
+    var rightNumber = "0";
+    var leftNumber = "0";
+    var result = "0";
+    var __op = Operation.Empty;
+
+    
+    
+    var equalPressed = false
+    var calculatorTurnedOn = true
+    var pressedFlag = -1 // 1= number ,2 = operation ,3 = equals
+    
     
     
     @IBAction func onNumberPressedPlay(sender: UIButton){
         
         
-        if equalPressed == true {
-            counterLabel.text = ""
-           // result = ""
-           // leftNumber = "0"
-            equalPressed = false
-        
-            
-            if pressedFlag != 2 {
-                leftNumber = "0"
-                result = ""
-            }
-            
-        }
-        
-        
-        
-        pressedFlag = 1
         
         presentNumber += "\(sender.tag)"
         
@@ -89,12 +89,26 @@ class ViewController: UIViewController {
         playSound()
         
         
+        pressedFlag = 1 // number pressed
+        
     }
 
     
     @IBAction func onEqualPress(sender: UIButton){
-          print ("\n__TAG equal\(presentNumber)\n")
-        equalPressed = true
+        
+        
+        
+        
+        if pressedFlag == 1{
+            pressedFlag = 3 // number before equal
+        }
+        else if pressedFlag == 2{
+            pressedFlag = 5 // operator before equal
+        }
+        else if pressedFlag == -1 {
+            pressedFlag = 6 // equal pressed first time
+        }
+        
         processEqual()
         
     }
@@ -138,210 +152,17 @@ class ViewController: UIViewController {
     }
     
     
-    var currentOperation = Operation.Empty
-    var rightNumber = "";
-    var leftNumber = "0";
-    var result = "";
-    
-    var __op = Operation.Empty;
+   
     
     
     
-    func processEqual(){
-        
-       
-        
-        print ("\npresent = \(presentNumber) \nresult=\(result) ")
-        
-
-        
-        //counterLabel.text = result
-        //leftNumber = result
-        
-        //presentNumber = result;
-        
-        //equalPressed = true
-        
-        
-        
-        var op = __op;
-        
-        if op != Operation.Empty {
-            
-                __op = Operation.Empty
-            
-                counterLabel.text = ""
-                
-                
-                
-                if presentNumber != ""  {
-                    
-                    
-                    
-                    rightNumber = presentNumber
-                    
-                    
-                    
-                    print ("\npresent = \(presentNumber) \nright=\(rightNumber) \nleft = \(leftNumber)")
-                    
-                    
-                    
-                    if op == Operation.Add {
-                        result  = "\(Double(leftNumber)! + (Double(rightNumber))!)"
-                        
-                        op = Operation.Empty
-                        
-                    }else if op == Operation.Subtract {
-                        
-                        op = Operation.Empty
-                        
-                        
-                        result  = "\(Double(leftNumber)! - (Double(rightNumber))!)"
-                        
-                    }
-                    else if op == Operation.Multiply {
-                        
-                        op = Operation.Empty
-                        
-                        result  = "\(Double(leftNumber)! * (Double(rightNumber))!)"
-                        
-                    }
-                    else if op == Operation.Division{
-                        
-                        op = Operation.Empty
-                        
-                        let tempnum = (Double(rightNumber))!
-                        
-                        
-                        if tempnum != 0{
-                            result  = "\(Double(leftNumber)! / (Double(rightNumber))!)"
-                        }
-                        else {
-                            result = "INF"
-                        }
-                        
-                        
-                    }
-                    
-                    
-                    counterLabel.text = result
-                    leftNumber = result
-                    
-                    
-                    presentNumber = "";
-                    
-                    
-                    currentOperation = Operation.Empty;
-                    
-                }
-                
-                
-                
-        }
-        
-    }
+    func processEqual(){}
     
     
     
     
     
-    func processOperation(operation :Operation){
-        
-        
-        pressedFlag = 2
-        
-        var op = operation;
-        
-        if op != Operation.Empty{
-
-            __op = operation
-            
-            counterLabel.text = ""
-
-            
-            
-            if leftNumber != ""  && presentNumber != ""{
-                
-                
-                
-                rightNumber = presentNumber
-                
-           
-                
-                print ("\npresent = \(presentNumber) \nright=\(rightNumber) \nleft = \(leftNumber)")
-                
-                
-                
-                if op == Operation.Add {
-                    result  = "\(Double(leftNumber)! + (Double(rightNumber))!)"
-                    
-                    op = Operation.Empty
-                    
-                }else if op == Operation.Subtract {
-                    
-                    op = Operation.Empty
-                    
-                    
-                    result  = "\(Double(leftNumber)! - (Double(rightNumber))!)"
-                    
-                }
-                else if op == Operation.Multiply {
-                    
-                    op = Operation.Empty
-                    
-                    if calculatorTurnedOn == true {
-                        calculatorTurnedOn = false
-                        result = "\(Double(rightNumber)!)"
-                        
-                    }else {
-                        
-                        result  = "\(Double(leftNumber)! * (Double(rightNumber))!)"
-                    }
-                }
-                else if op == Operation.Division{
-                    
-                    op = Operation.Empty
-                    
-                    let tempnum = (Double(rightNumber))!
-                    
-                    
-                    if tempnum != 0{
-                        
-                        if calculatorTurnedOn == true {
-                            calculatorTurnedOn = false
-                            result = "\(Double(rightNumber)!)"
-                            
-                        }else {
-                            result  = "\(Double(leftNumber)! / (Double(rightNumber))!)"
-                        }
-                        
-                       
-                    }
-                    else {
-                        result = "INF"
-                    }
-                    
-                    
-                }
-                
-                
-                counterLabel.text = result
-                leftNumber = result
-                
-                
-                presentNumber = "";
-                
-                
-                currentOperation = Operation.Empty;
-                
-             }
-            
-            
-            
-        }
-        
-        
-    }
+    func processOperation(operation :Operation){}
     
     
     
